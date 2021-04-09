6271ed7ca7167757dc3c063be4561a847ed41a17
Baskar5566 <malayalaabaskar6@gmail.com>
2021-04-09 12:18:33 +0530

    gitsample

--- src/org/github/HubDemo.java
+++ src/org/github/HubDemo.java
@@ -13,6 +13,16 @@ public class HubDemo {
 	private void method3() {
 		System.out.println("method 3");
 	}
+	private void method4() {
+        System.out.println(4);
+		}
+		private void method5() {
+     System.out.println(5);
+		}
+		private void method6() {
+     System.out.println(6);
+		}
+		
 
 }
 
